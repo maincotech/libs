11e2935f8c8cc085b7ce0ac556e97cb5d9803403
maincotech <minghl@maincotech.com>
2020-10-12 23:27:54 +0900

    Add some extensions

---
+++ Maincotechlibs/Maincotech.Common/src/Localization/ILanguageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Maincotech.Localization
+{
+    public interface ILanguageService
+    {
+        CultureInfo CurrentCulture { get; }
+
+        Resources Resources { get; }
+
+        event EventHandler<CultureInfo> LanguageChanged;
+
+        void SetLanguage(CultureInfo culture);
+    }
+}
